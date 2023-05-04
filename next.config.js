98439db994c8ae95dac5add0f89bc10b06eb5211
Samer Ibrahimovic <sameribrahimovic@hotmail.com>
2023-05-04 14:59:42 +0200

    next config domain changed

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: [
-      "localhost",
+      //"localhost",
+      "https://sm-youtube-clone.vercel.app/",
       "bootcamp-samco.s3.eu-north-1.amazonaws.com",
       "bootcamp-samco.s3.amazonaws.com",
     ],
